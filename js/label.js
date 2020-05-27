8619fec8137c28403b1de649e68e8446d1a9eff3
心叶 <1329132389@qq.com>
2020-05-27 11:17:52 +0800

    初始化标签和搜索页面，修改部分页面样式

---
+++ js/label.js
@@ -0,0 +1,30 @@
+;$('body').ready(function() {
+
+    layui.use(['jquery','layer', 'laytpl'],function(){
+        var $ = layui.$,
+            layer = layui.layer;
+        laytpl = layui.laytpl;
+    });
+
+    let data = { //数据
+        "title":"标签标题",
+        "list":[
+            '标签111', '标签2', '标签2424243', "标签4",
+            '标签111', '标签2', '标签2424243', "标签4",
+            '标签111', '标签2', '标签2424243', "标签4"
+        ]
+    }
+    let getTpl = label.innerHTML
+        ,view = document.getElementById('label_view');
+    laytpl(getTpl).render(data, function(html){
+        view.innerHTML = html;
+    });
+
+    var tags_a = $("#tags span");
+    tags_a.each(function(){
+        var x = 20;
+        var y = 0;
+        var rand = parseInt(Math.random() * (x - y + 1) + y);
+        $(this).addClass("tags"+rand);
+    });
+})
